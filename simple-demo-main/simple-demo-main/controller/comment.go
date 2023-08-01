d613756f1f0d4242fabc5f446edc11b90744fe76
for shell <fromShell@paas.com>
2023-08-01 23:17:59 +0800

    add .

--- simple-demo-main/simple-demo-main/controller/comment.go
+++ simple-demo-main/simple-demo-main/controller/comment.go
@@ -10,6 +10,7 @@ type CommentListResponse struct {
 	CommentList []Comment `json:"comment_list,omitempty"`
 }
 
+
 type CommentActionResponse struct {
 	Response
 	Comment Comment `json:"comment,omitempty"`
--- simple-demo-main/simple-demo-main/test/base_api_test.go
+++ simple-demo-main/simple-demo-main/test/base_api_test.go
@@ -8,14 +8,20 @@ import (
 	"time"
 )
 
+// 这是一个示例的Go代码片段
+
 func TestFeed(t *testing.T) {
+	// 创建一个期望对象
 	e := newExpect(t)
 
+	// 发送GET请求，获取/feed/接口的响应
 	feedResp := e.GET("/douyin/feed/").Expect().Status(http.StatusOK).JSON().Object()
 	feedResp.Value("status_code").Number().Equal(0)
 	feedResp.Value("video_list").Array().Length().Gt(0)
 
+	// 遍历视频列表
 	for _, element := range feedResp.Value("video_list").Array().Iter() {
+		// 获取视频对象
 		video := element.Object()
 		video.ContainsKey("id")
 		video.ContainsKey("author")
@@ -25,11 +31,14 @@ func TestFeed(t *testing.T) {
 }
 
 func TestUserAction(t *testing.T) {
+	// 创建一个期望对象
 	e := newExpect(t)
 
+	// 生成一个随机的注册值
 	rand.Seed(time.Now().UnixNano())
 	registerValue := fmt.Sprintf("douyin%d", rand.Intn(65536))
 
+	// 发送POST请求，进行用户注册
 	registerResp := e.POST("/douyin/user/register/").
 		WithQuery("username", registerValue).WithQuery("password", registerValue).
 		WithFormField("username", registerValue).WithFormField("password", registerValue).
@@ -40,6 +49,7 @@ func TestUserAction(t *testing.T) {
 	registerResp.Value("user_id").Number().Gt(0)
 	registerResp.Value("token").String().Length().Gt(0)
 
+	// 发送POST请求，进行用户登录
 	loginResp := e.POST("/douyin/user/login/").
 		WithQuery("username", registerValue).WithQuery("password", registerValue).
 		WithFormField("username", registerValue).WithFormField("password", registerValue).
@@ -50,47 +60,7 @@ func TestUserAction(t *testing.T) {
 	loginResp.Value("user_id").Number().Gt(0)
 	loginResp.Value("token").String().Length().Gt(0)
 
+	// 获取登录响应中的token
 	token := loginResp.Value("token").String().Raw()
-	userResp := e.GET("/douyin/user/").
-		WithQuery("token", token).
-		Expect().
-		Status(http.StatusOK).
-		JSON().Object()
-	userResp.Value("status_code").Number().Equal(0)
-	userInfo := userResp.Value("user").Object()
-	userInfo.NotEmpty()
-	userInfo.Value("id").Number().Gt(0)
-	userInfo.Value("name").String().Length().Gt(0)
-}
 
-func TestPublish(t *testing.T) {
-	e := newExpect(t)
-
-	userId, token := getTestUserToken(testUserA, e)
-
-	publishResp := e.POST("/douyin/publish/action/").
-		WithMultipart().
-		WithFile("data", "../public/bear.mp4").
-		WithFormField("token", token).
-		WithFormField("title", "Bear").
-		Expect().
-		Status(http.StatusOK).
-		JSON().Object()
-	publishResp.Value("status_code").Number().Equal(0)
-
-	publishListResp := e.GET("/douyin/publish/list/").
-		WithQuery("user_id", userId).WithQuery("token", token).
-		Expect().
-		Status(http.StatusOK).
-		JSON().Object()
-	publishListResp.Value("status_code").Number().Equal(0)
-	publishListResp.Value("video_list").Array().Length().Gt(0)
-
-	for _, element := range publishListResp.Value("video_list").Array().Iter() {
-		video := element.Object()
-		video.ContainsKey("id")
-		video.ContainsKey("author")
-		video.Value("play_url").String().NotEmpty()
-		video.Value("cover_url").String().NotEmpty()
-	}
-}
+	
